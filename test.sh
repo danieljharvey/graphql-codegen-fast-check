7e6a5c8ff2bbf6c00ab1fd87e562057e805c2e6c
Daniel Harvey <danieljamesharvey@gmail.com>
2020-04-01 09:28:19 +0100

    Get basic tests working

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# lets set up a test environment
+# die if anything goes wrong
+set -euxo pipefail
 
+# compile typescript
 yarn build
 
-graphql-codegen --config ./test/codegen.yml
-
-yarn jest -c jest.tsc.config.js
+# run codegen
+yarn graphql-codegen --verbose --config ./test/codegen.yml
